e76e7c1da49084eb10039f0ee5d25f20ef373476
FlameFenix <68155186+FlameFenix@users.noreply.github.com>
2020-12-03 21:06:03 +0200

    .

---
+++ First Steps In Coding - Exercise/08. Fish Tank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _08._Fish_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double weidth = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double aquaSize = lenght * weidth * height;
+            double littersInAqua = aquaSize * 0.001;
+            double percentAgain = percent * 0.01;
+            double littersNeed = littersInAqua * (1 - percentAgain);
+
+            Console.WriteLine($"{littersNeed}");
+        }
+    }
+}
