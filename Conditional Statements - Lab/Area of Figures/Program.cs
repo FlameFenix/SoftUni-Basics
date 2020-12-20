98379797b18d25a048b85680ee054e7e783c2586
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2020-12-20 16:34:00 +0200

    lab

---
+++ Conditional Statements - Lab/Area of Figures/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Area_of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            double area = 0;
+
+            if (type == "square")
+            {
+                double square = double.Parse(Console.ReadLine());
+                area = square * square;
+            }
+
+            else if (type == "rectangle")
+            {
+                double lenght = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                area = lenght * width;
+            }
+            else if (type == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                area = radius * radius * Math.PI;
+            }
+
+            else if (type == "triangle")
+            {
+                double basee = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                area = basee * height / 2;
+            }
+
+            Console.WriteLine($"{area:F3}");
+        }
+    }
+}
---
+++ Conditional Statements - Lab/Toy Shop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Toy_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceNeeded = double.Parse(Console.ReadLine());
+            int puzzles = int.Parse(Console.ReadLine());
+            int dolls = int.Parse(Console.ReadLine());
+            int bears = int.Parse(Console.ReadLine());
+            int minions = int.Parse(Console.ReadLine());
+            int trucks = int.Parse(Console.ReadLine());
+
+            double puzzlesPrice = puzzles * 2.60;
+            double dollsPrice = dolls * 3;
+            double bearsPrice = bears * 4.10;
+            double minionsPrice = minions * 8.20;
+            double trucksPrice = trucks * 2;
+
+            double sum = puzzlesPrice + dollsPrice + bearsPrice + minionsPrice + trucksPrice;
+            int numberOfToys = puzzles + dolls + bears + minions + trucks;
+
+            if (numberOfToys >= 50)
+            {
+                sum = sum * 0.75;
+            }
+
+            sum = sum * 0.9;
+
+            if (sum >= priceNeeded)
+            {
+                double VacationMoney = sum - priceNeeded;
+                Console.WriteLine($"Yes! {VacationMoney:F2} lv left.");
+            }
+            else
+            {
+                double neededMoney = priceNeeded - sum;
+                Console.WriteLine($"Not enough money! {neededMoney:F2} lv needed.");
+            }
+        }
+    }
+}
