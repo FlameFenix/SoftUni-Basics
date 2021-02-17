c93619439a9fc738d1e09f683f1ebdec478b7817
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-17 23:24:16 +0200

    Lab

---
+++ JavaScript/Nested Loops - Lab/Clock.js
@@ -0,0 +1,11 @@
+function Clock() {
+
+    for (let hours = 0; hours <= 23; hours++) {
+
+        for (let  minutes = 0;  minutes <= 59; minutes++) {
+            console.log(`${hours}:${minutes}`);
+        }
+    }
+}
+
+Clock();
