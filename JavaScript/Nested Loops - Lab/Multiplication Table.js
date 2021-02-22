5a905534db60fa77382a13ba125df5ece47d5570
Yordan Dimitrov <68155186+FlameFenix@users.noreply.github.com>
2021-02-22 23:14:14 +0200

    Lab

---
+++ JavaScript/Nested Loops - Lab/Multiplication Table.js
@@ -0,0 +1,13 @@
+function MultiplicationTable() {
+
+    for (let index = 1; index <= 10; index++) {
+
+        for (let IndexTwo = 1; IndexTwo <= 10; IndexTwo++) {
+            console.log(`${index} * ${IndexTwo} = ${index * IndexTwo}`);
+        }
+
+    }
+
+}
+
+MultiplicationTable();
